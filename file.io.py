441ea745fdc7870f496e0ff537bf37fe6f0d19fd
79252 <jackstreymon@gmail.com>
2024-11-10 11:33:34 +0300

    Заготовка проекта с главным окном программы и функцией upload для загрузки файла на сайт file.io

---
+++ file.io.py
@@ -0,0 +1,29 @@
+from importlib.metadata import files
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog as fd
+import requests
+
+
+def upload():
+    filepath = fd.askopenfilename()
+    if filepath:
+        files = {"file": open(filepath, "rb")}
+        response = requests.post("https://file.io", files = files)
+        if response.status_code == 200:
+            link = response.json()["link"]
+            entry.insert(0, link)
+
+
+window = Tk()
+window.title("Сохранение файлов в облаке")
+window.geometry("400x200")
+
+button = ttk.Button(text = "Загрузить файл", command = upload)
+button.pack()
+
+entry = ttk.Entry()
+entry.pack()
+
+
+window.mainloop()
